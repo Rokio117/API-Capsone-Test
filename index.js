8f5db9456c54efffc8d516ca78b205b7dfb99a08
Nick Hillary <hillan02@gmail.com>
2019-06-28 21:02:29 -0500

    finished movie API fetches

---
+++ index.js
@@ -0,0 +1,95 @@
+const movieApiKey = 'api_key=f565fbe8e4c3d996435b7ecb4cbacbc0'
+const movieBaseUrl = 'https://api.themoviedb.org/3/search/movie'
+const movieSearchUrl = 'https://api.themoviedb.org/3/movie/'
+const movieAdditionalQs = '&page=1&include_adult=false'
+const posterBaseUrl = 'https://image.tmdb.org/t/p/w500/'
+
+const songBaseUrl ='http://ws.audioscrobbler.com/2.0/?method=track.search&track='
+const songApiKey ='&api_key=5693fd68291fa577eff3066dbff9bbc2'
+const songAdditionalQs ='&format=json'
+
+
+
+function handleMovieSearch() {
+  $('#movie-form').submit(function(event){
+    event.preventDefault()
+    movieSearch = $('#movie-value').val()
+    encodedMovieSearch = encodeURIComponent(movieSearch)
+    fetchMovieData(encodedMovieSearch)
+  })
+
+}
+function fetchMovieData(movieSearchData) {
+  url = `${movieBaseUrl}?${movieApiKey}&language=en-US&query=${movieSearchData}${movieAdditionalQs}`
+  fetch(url)
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(responseJson) {
+    getSpecificMovie(responseJson)
+  })
+}
+function getSpecificMovie(data) {
+  movieId = data.results[0].id
+  idUrl = `${movieSearchUrl}${movieId}?${movieApiKey}&language=en-US&append_to_response=similar,credits`
+  fetch(idUrl)
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(responseJson){
+    console.log(responseJson)
+    displayMovieData(responseJson)
+  })
+}
+
+function displayMovieData(data) {
+  console.log(data.release_date)
+  displayMovieImage(data)
+  console.log(data.credits.crew[0].name)
+  console.log(data.credits.cast[0].name)
+  console.log(data.credits.cast[1].name)
+  console.log(data.credits.cast[2].name)
+  console.log(data.overview)
+  console.log(data.similar.results[0].title)
+  console.log(data.similar.results[1].title)
+  console.log(data.similar.results[2].title)
+}
+function displayMovieImage(imageData){
+  posterPath = imageData.poster_path
+  posterUrl = `${posterBaseUrl}${posterPath}`
+  $('#results-section').append(`<img src='${posterUrl}'></img>`)
+}
+  
+
+function handleSongSearch() {
+$('#song-form').submit(function(event){
+  event.preventDefault()
+  songSearch = $('#song-value').val()
+  encodedSongSearch = encodeURIComponent(songSearch)
+  fetchSongData(encodedSongSearch)
+})
+}
+
+function fetchSongData(songData){
+  url = `${songBaseUrl}${songData}${songApiKey}${songAdditionalQs}`
+  fetch(url)
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(responseJson){
+    songData = responseJson
+    displayMusicData(songData)
+  })
+}
+
+function displayMusicData(musicData){
+  console.log(musicData.results.trackmatches.track[0].artist)
+  console.log(musicData.results.trackmatches.track[0].name)
+}
+
+function callHandles() {
+  handleMovieSearch()
+  handleSongSearch()
+}
+
+$(callHandles)
